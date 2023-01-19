ff0df37c5db9bd40ad22bf6b26ee611d80ccb2c5
하훈목 <hunmok1027@gmail.com>
2023-01-19 16:27:56 +0900

    feat:google map link 추가

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       }
     );
   };
-  
+
   console.log(coord);
 
   return (
@@ -30,6 +30,7 @@ function App() {
       <button onClick={getCurrentPosition}>현재위치기록</button>
       <h1>App</h1>
       <Link to={'/hit-test'}>to HitTest</Link>
+      <Link to={'/three'}>to googleMap</Link>
       {log.map((el: any, i: number) => (
         <div key={i}>
           latitude:{el.latitude} longitude:{el.longitude}
