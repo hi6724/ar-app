68af3fe080ea1cbc5aaf2944fc3b175f62532ac5
하훈목 <hunmok1027@gmail.com>
2023-01-20 00:34:47 +0900

    tree 여러개 렌더링

--- src/pages/ThreeGlobe.tsx
+++ src/pages/ThreeGlobe.tsx
@@ -1,18 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {
-  AmbientLight,
-  BoxGeometry,
-  DirectionalLight,
-  MathUtils,
-  Matrix4,
-  MeshNormalMaterial,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-} from 'three';
+
 import * as THREE from 'three';
 import { latLngToVector3, ThreeJSOverlayView } from '@googlemaps/three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils';
 // import stickMan from '../assets/scene.gltf';
 
 const MAP_OPTION: google.maps.MapOptions = {
@@ -28,163 +19,90 @@ const MAP_OPTION: google.maps.MapOptions = {
 function ThreeGlobe() {
   const ref = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<google.maps.Map>();
+  async function initMap() {
+    if (!ref.current) return;
+    let userModel: any;
+    const temp: any = new google.maps.Map(ref.current, MAP_OPTION);
+    const scene = new THREE.Scene();
+    const clock = new THREE.Clock();
+    let mixer: any;
+    new ThreeJSOverlayView({
+      scene,
+      map: temp,
+      THREE,
+    });
 
-  useEffect(() => {
-    // window.addEventListener(
-    //   'deviceorientationabsolute',
-    //   (e: any) => {
-    //     compass = 360 - e.alpha;
-    //   },
-    //   true
-    // );
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
+    directionalLight.position.set(0, 10, 50);
+    scene.add(directionalLight);
 
-    function initWebglOverlayView(map: any) {
-      let scene: any, renderer: any, camera: any, loader: any;
-
-      const webglOverlayView = new google.maps.WebGLOverlayView();
-      webglOverlayView.onAdd = () => {
-        // Set up the scene.
-        scene = new Scene();
-        camera = new PerspectiveCamera();
-
-        const ambientLight = new AmbientLight(0xffffff, 0.75); // Soft white light.
-        scene.add(ambientLight);
-        const directionalLight = new DirectionalLight(0xffffff, 0.25);
-        directionalLight.position.set(0.5, -1, 0.5);
-        scene.add(directionalLight);
-
-        loader = new GLTFLoader();
-        loader.load('/stickman/scene.gltf', (gltf: any) => {
-          model = gltf;
-          gltf.scene.scale.set(300, 300, 300);
-          gltf.scene.rotation.x = Math.PI / 2;
-          scene.add(gltf.scene);
-
-          mixer = new THREE.AnimationMixer(gltf.scene);
-          const clip = THREE.AnimationClip.findByName(gltf.animations, 'Idle');
-          mixer.clipAction(clip).play();
-        });
-
-        loader.load('/tree/scene.gltf', (gltf: any) => {
-          model = gltf;
-          gltf.scene.scale.set(10, 10, 10);
-          gltf.scene.rotation.x = Math.PI / 2;
-          scene.add(gltf.scene);
-        });
-      };
-
-      webglOverlayView.onContextRestored = ({ gl }) => {
-        renderer = new WebGLRenderer({
-          canvas: gl.canvas,
-          context: gl,
-          ...gl.getContextAttributes(),
-        });
-        renderer.autoClear = false;
-        // Wait to move the camera until the 3D model loads.
-
-        const box = new THREE.Mesh(
-          new BoxGeometry(10, 500, 10),
-          new MeshNormalMaterial()
-        );
-        box.position.copy(
-          latLngToVector3({ lat: 37.4935145, lng: 126.7567966 })
-        );
-        scene.add(box);
-
-        loader.manager.onLoad = () => {
-          const clock = new THREE.Clock();
-          // console.log('hh');
-
-          renderer.setAnimationLoop((e: any) => {
-            console.log(compass);
-            model.scene.rotation.y = (Math.PI / 180) * compass;
-
-            mixer.update(clock.getDelta());
-            if (!MAP_OPTION.center?.lat) return;
-            MAP_OPTION.center.lat = Number(MAP_OPTION.center?.lat) + 0.000001;
-
-            // navigator.geolocation.getCurrentPosition(
-            //   ({ coords: { latitude, longitude } }) => {
-            //     MAP_OPTION.center = { lat: latitude, lng: longitude };
-            //   }
-            // );
-
-            webglOverlayView.requestRedraw();
-          });
-        };
-      };
-
-      webglOverlayView.onDraw = ({ gl, transformer }) => {
-        // center: { lat: 37.4935145, lng: 126.7567966 },
-
-        const latLngAltitudeLiteral: any = {
-          lat: MAP_OPTION!.center!.lat,
-          lng: MAP_OPTION!.center!.lng,
-          altitude: 10,
-        };
-
-        const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
-
-        camera.projectionMatrix = new Matrix4().fromArray(matrix);
-        webglOverlayView.requestRedraw();
-        renderer.render(scene, camera);
-        renderer.resetState();
-      };
-
-      webglOverlayView.setMap(map);
-    }
-
-    if (ref.current && !map) {
-      const temp: any = new google.maps.Map(ref.current, MAP_OPTION);
-      const scene = new THREE.Scene();
-      const clock = new THREE.Clock();
-      let mixer: any;
-      new ThreeJSOverlayView({
-        scene,
-        map: temp,
-        THREE,
-      });
-
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
-      scene.add(ambientLight);
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
-      directionalLight.position.set(0, 10, 50);
-      scene.add(directionalLight);
-
-      const loader = new GLTFLoader();
-      const url = '/stickman/scene.gltf';
-      loader.load(url, (gltf) => {
-        gltf.scene.scale.set(100, 100, 100);
-        gltf.scene.position.copy(
-          latLngToVector3({ lat: 37.4935145, lng: 126.7567966 })
-        );
-        gltf.scene.position.setY(100);
-        mixer = new THREE.AnimationMixer(gltf.scene);
-        const clip = THREE.AnimationClip.findByName(gltf.animations, 'Idle');
-        mixer.clipAction(clip).play();
-        scene.add(gltf.scene);
-      });
-
-      let mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(10, 200, 10),
-        new MeshNormalMaterial()
+    // user 렌더링
+    const loader = new GLTFLoader();
+    const url = '/stickman/scene.gltf';
+    loader.load(url, (gltf) => {
+      userModel = gltf.scene;
+      gltf.scene.scale.set(100, 100, 100);
+      gltf.scene.position.copy(
+        latLngToVector3({ lat: 37.4935145, lng: 126.7567966 })
       );
-      [1, 2, 3, 4, 5].forEach((el) => {
-        const box = mesh.clone();
-        box.position.copy(
+      gltf.scene.position.setY(100);
+      mixer = new THREE.AnimationMixer(gltf.scene);
+      const clip = THREE.AnimationClip.findByName(gltf.animations, 'Run');
+      mixer.clipAction(clip).play();
+      scene.add(gltf.scene);
+    });
+
+    // 빌딩 렌더링
+
+    const treeUrl = '/tree/scene.gltf';
+    let treeModel: any;
+    treeModel = await loader.loadAsync(treeUrl);
+
+    let mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(10, 200, 10),
+      new THREE.MeshNormalMaterial()
+    );
+
+    [1, 2, 3, 4, 5].forEach((el) => {
+      console.log('loop', el, treeModel);
+      if (treeModel) {
+        const tree = SkeletonUtils.clone(treeModel.scene);
+        tree.position.copy(
           latLngToVector3({ lat: 37.4935145 + el / 1000, lng: 126.7567966 })
         );
-        box.position.setY(25);
-        scene.add(box);
-      });
+        tree.position.setY(25);
+        scene.add(tree);
+      }
+    });
 
-      const animate = () => {
-        mixer?.update(clock.getDelta());
-        requestAnimationFrame(animate);
-      };
+    let prevPos = { lat: 37.4935145, lng: 126.7567966 };
+    const animate = () => {
+      mixer?.update(clock.getDelta());
+      const newPos = { lat: prevPos.lat - 0.000001, lng: 126.7567966 };
+      prevPos = newPos;
+      userModel?.position.copy(latLngToVector3(newPos));
+      // console.log(userModel.position);
       requestAnimationFrame(animate);
+    };
+    requestAnimationFrame(animate);
 
-      setMap(temp);
+    setMap(temp);
+  }
+
+  useEffect(() => {
+    let compass: any;
+    window.addEventListener(
+      'deviceorientationabsolute',
+      (e: any) => {
+        compass = 360 - e.alpha;
+      },
+      true
+    );
+
+    if (ref.current && !map) {
+      initMap();
     }
   }, []);
 
--- src/pages/temp.tsx
+++ src/pages/temp.tsx
@@ -1,18 +1,18 @@
+
+
+
 import React, { useEffect, useRef, useState } from 'react';
 import {
   AmbientLight,
-  BoxGeometry,
   DirectionalLight,
-  MathUtils,
   Matrix4,
-  MeshNormalMaterial,
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
 } from 'three';
 import * as THREE from 'three';
-import { latLngToVector3, ThreeJSOverlayView } from '@googlemaps/three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { latLngToVector3 } from '@googlemaps/three';
 // import stickMan from '../assets/scene.gltf';
 
 const MAP_OPTION: google.maps.MapOptions = {
@@ -30,46 +30,42 @@ function ThreeGlobe() {
   const [map, setMap] = useState<google.maps.Map>();
 
   useEffect(() => {
-    // window.addEventListener(
-    //   'deviceorientationabsolute',
-    //   (e: any) => {
-    //     compass = 360 - e.alpha;
-    //   },
-    //   true
-    // );
-
+    let mixer: any;
     function initWebglOverlayView(map: any) {
       let scene: any, renderer: any, camera: any, loader: any;
-
       const webglOverlayView = new google.maps.WebGLOverlayView();
+
       webglOverlayView.onAdd = () => {
-        // Set up the scene.
         scene = new Scene();
         camera = new PerspectiveCamera();
-
         const ambientLight = new AmbientLight(0xffffff, 0.75); // Soft white light.
         scene.add(ambientLight);
         const directionalLight = new DirectionalLight(0xffffff, 0.25);
         directionalLight.position.set(0.5, -1, 0.5);
         scene.add(directionalLight);
 
+        // 유저 에셋 로드
         loader = new GLTFLoader();
         loader.load('/stickman/scene.gltf', (gltf: any) => {
-          model = gltf;
-          gltf.scene.scale.set(300, 300, 300);
+          gltf.scene.scale.set(100, 100, 100);
           gltf.scene.rotation.x = Math.PI / 2;
-          scene.add(gltf.scene);
-
           mixer = new THREE.AnimationMixer(gltf.scene);
           const clip = THREE.AnimationClip.findByName(gltf.animations, 'Idle');
           mixer.clipAction(clip).play();
-        });
-
-        loader.load('/tree/scene.gltf', (gltf: any) => {
-          model = gltf;
-          gltf.scene.scale.set(10, 10, 10);
-          gltf.scene.rotation.x = Math.PI / 2;
           scene.add(gltf.scene);
+        });
+        // 캡슐 에셋 로드
+        let mesh = new THREE.Mesh(
+          new THREE.BoxGeometry(10, 200, 10),
+          new THREE.MeshNormalMaterial()
+        );
+        [1, 2, 3, 4, 5].forEach((el) => {
+          const box = mesh.clone();
+          box.position.copy(
+            latLngToVector3({ lat: 37.4935145 + el / 1000, lng: 126.7567966 })
+          );
+          box.position.setY(25);
+          scene.add(box);
         });
       };
 
@@ -80,43 +76,27 @@ function ThreeGlobe() {
           ...gl.getContextAttributes(),
         });
         renderer.autoClear = false;
-        // Wait to move the camera until the 3D model loads.
-
-        const box = new THREE.Mesh(
-          new BoxGeometry(10, 500, 10),
-          new MeshNormalMaterial()
-        );
-        box.position.copy(
-          latLngToVector3({ lat: 37.4935145, lng: 126.7567966 })
-        );
-        scene.add(box);
 
         loader.manager.onLoad = () => {
           const clock = new THREE.Clock();
-          // console.log('hh');
-
           renderer.setAnimationLoop((e: any) => {
-            console.log(compass);
-            model.scene.rotation.y = (Math.PI / 180) * compass;
-
             mixer.update(clock.getDelta());
             if (!MAP_OPTION.center?.lat) return;
-            MAP_OPTION.center.lat = Number(MAP_OPTION.center?.lat) + 0.000001;
 
-            // navigator.geolocation.getCurrentPosition(
-            //   ({ coords: { latitude, longitude } }) => {
-            //     MAP_OPTION.center = { lat: latitude, lng: longitude };
-            //   }
-            // );
+            navigator.geolocation.getCurrentPosition(
+              ({ coords: { latitude, longitude } }) => {
+                MAP_OPTION.center = { lat: latitude, lng: longitude };
+              }
+            );
 
-            webglOverlayView.requestRedraw();
+            // webglOverlayView.requestRedraw();
+            // const { center } = MAP_OPTION;
+            // map.moveCamera({ center });
           });
         };
       };
 
       webglOverlayView.onDraw = ({ gl, transformer }) => {
-        // center: { lat: 37.4935145, lng: 126.7567966 },
-
         const latLngAltitudeLiteral: any = {
           lat: MAP_OPTION!.center!.lat,
           lng: MAP_OPTION!.center!.lng,
@@ -135,56 +115,9 @@ function ThreeGlobe() {
     }
 
     if (ref.current && !map) {
-      const temp: any = new google.maps.Map(ref.current, MAP_OPTION);
-      const scene = new THREE.Scene();
-      const clock = new THREE.Clock();
-      let mixer: any;
-      new ThreeJSOverlayView({
-        scene,
-        map: temp,
-        THREE,
-      });
-
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
-      scene.add(ambientLight);
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
-      directionalLight.position.set(0, 10, 50);
-      scene.add(directionalLight);
-
-      const loader = new GLTFLoader();
-      const url = '/stickman/scene.gltf';
-      loader.load(url, (gltf) => {
-        gltf.scene.scale.set(100, 100, 100);
-        gltf.scene.position.copy(
-          latLngToVector3({ lat: 37.4935145, lng: 126.7567966 })
-        );
-        gltf.scene.position.setY(100);
-        mixer = new THREE.AnimationMixer(gltf.scene);
-        const clip = THREE.AnimationClip.findByName(gltf.animations, 'Idle');
-        mixer.clipAction(clip).play();
-        scene.add(gltf.scene);
-      });
-
-      let mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(10, 200, 10),
-        new MeshNormalMaterial()
-      );
-      [1, 2, 3, 4, 5].forEach((el) => {
-        const box = mesh.clone();
-        box.position.copy(
-          latLngToVector3({ lat: 37.4935145 + el / 1000, lng: 126.7567966 })
-        );
-        box.position.setY(25);
-        scene.add(box);
-      });
-
-      const animate = () => {
-        mixer?.update(clock.getDelta());
-        requestAnimationFrame(animate);
-      };
-      requestAnimationFrame(animate);
-
+      const temp = new google.maps.Map(ref.current, MAP_OPTION);
       setMap(temp);
+      initWebglOverlayView(temp);
     }
   }, []);
 
