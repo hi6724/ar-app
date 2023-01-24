4853e4912d4ac51edaa15b5c78044aebf8d1d20a
하훈목 <hunmok1027@gmail.com>
2023-01-24 13:19:35 +0900

    bug-fix : test

--- src/pages/ThreeGlobe.tsx
+++ src/pages/ThreeGlobe.tsx
@@ -6,6 +6,7 @@ import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils';
 import { latLngToVector3Relative } from '@googlemaps/three';
 import { DB } from '../db';
 import { getDistance } from '../utils/calc';
+import { Link } from 'react-router-dom';
 // import stickMan from '../assets/scene.gltf';
 
 let CENTER = { lat: 37.493, lng: 126.756 };
@@ -15,22 +16,37 @@ const MAP_OPTION: google.maps.MapOptions = {
   heading: 20,
   zoom: 21,
   center: CENTER,
-  mapId: 'da92ac680ee5382b',
+  mapId: 'c93e550b0413e551',
   disableDefaultUI: true,
   keyboardShortcuts: false,
 };
 
 function ThreeGlobe() {
   const ref = useRef<HTMLDivElement>(null);
-  let scene: any, compass: any, map: any, userModel: any;
-  let mixer: any, locations: any;
+  let compass: any, map: any, userModel: any;
+  let locations: any,
+    boxes: any = [];
+
+  let mixer: THREE.AnimationMixer,
+    webglOverlayView: google.maps.WebGLOverlayView | null,
+    scene: THREE.Scene,
+    clock: THREE.Clock,
+    loader: GLTFLoader,
+    renderer: THREE.WebGLRenderer,
+    camera: THREE.PerspectiveCamera;
+
+  const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshNormalMaterial()
+  );
+  mesh.scale.set(10, 10, 100);
+  let isAdded = false;
   let prevPosition = { lat: 0, lng: 0 };
 
   useEffect(() => {
     function initWebglOverlayView(map: any) {
-      let renderer: any, camera: any, loader: any;
-      let isAdded = false;
-      const webglOverlayView = new google.maps.WebGLOverlayView();
+      if (webglOverlayView === null) return;
+      webglOverlayView = new google.maps.WebGLOverlayView();
       new google.maps.Marker({
         position: TEST_POS,
         map,
@@ -48,7 +64,7 @@ function ThreeGlobe() {
 
         // 유저 에셋 로드
         loader = new GLTFLoader();
-        loader.load('/stickman/scene.gltf', (gltf: any) => {
+        loader.load('/stickman/scene.gltf', (gltf) => {
           userModel = gltf.scene;
           userModel.scale.set(10, 10, 10);
           userModel.rotation.x = Math.PI / 2;
@@ -59,12 +75,11 @@ function ThreeGlobe() {
         });
         // 애니메이션 추가
         loader.manager.onLoad = () => {
-          const clock = new THREE.Clock();
+          clock = new THREE.Clock();
           renderer.setAnimationLoop((e: any) => {
             userModel.rotation.y = (Math.PI * compass) / 180;
             mixer?.update(clock.getDelta());
             if (!MAP_OPTION.center?.lat) return;
-            webglOverlayView.requestRedraw();
           });
         };
       };
@@ -73,19 +88,14 @@ function ThreeGlobe() {
         renderer = new THREE.WebGLRenderer({
           canvas: gl.canvas,
           context: gl,
+          powerPreference: 'low-power',
           ...gl.getContextAttributes(),
         });
         renderer.autoClear = false;
       };
 
-      let boxes: any = [];
-      const mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(),
-        new THREE.MeshNormalMaterial()
-      );
-      mesh.scale.set(10, 10, 100);
-
-      webglOverlayView.onDraw = async ({ gl, transformer }) => {
+      webglOverlayView.onDraw = async ({ transformer }) => {
+        console.log('draw');
         const pos = { ...CENTER, altitude: 0 };
         navigator.geolocation.getCurrentPosition(
           async ({ coords: { latitude, longitude } }) => {
@@ -101,20 +111,18 @@ function ThreeGlobe() {
               lat2: latitude,
               lon2: longitude,
             });
-
-            // 1m 이상일 때 UI 업데이트
-            if (distance > 0.001) {
+            // 10cm 이상일 때 UI 업데이트
+            if (distance > 0.0001) {
+              // alert(distance);
               CENTER = { lat: latitude, lng: longitude };
               alert('MOVED!');
             }
             //1km 이상일 때 API 다시 호출
-            if (apiDistance > 0.01) {
-              // alert('API 최신화');
+            if (apiDistance > 1) {
               prevPosition = { lat: latitude, lng: longitude };
-              boxes = [];
               const { results } = await (
                 await fetch(
-                  'https://api.foursquare.com/v3/places/search?ll=37.475007%2C126.7698833&radius=1000',
+                  `https://api.foursquare.com/v3/places/search?ll=${latitude}%2C${longitude}&radius=1000`,
                   {
                     headers: {
                       Authorization:
@@ -129,14 +137,21 @@ function ThreeGlobe() {
           }
         );
         if (!isAdded) {
-          console.log(locations);
-          locations?.forEach(({ geocodes }: any) => {
+          boxes.forEach(({ box }: any) => {
+            const selectedObject = scene.getObjectByName(box.name);
+            if (selectedObject) {
+              scene.remove(selectedObject);
+            }
+          });
+
+          locations?.forEach(({ geocodes, fsq_id }: any) => {
             const box = mesh.clone();
             const location = {
               lat: geocodes.main.latitude,
               lng: geocodes.main.longitude,
             };
             boxes.push({ box, location });
+            box.name = fsq_id;
             scene.add(box);
           });
           isAdded = true;
@@ -145,38 +160,40 @@ function ThreeGlobe() {
           const { x, z } = latLngToVector3Relative(pos, location);
           box.position.set(-x * 0.7934375, z * 0.7934375, 0);
         });
-        const { x, z } = latLngToVector3Relative(pos, TEST_POS);
-        mesh.position.set(-x * 0.7934375, z * 0.7934375, 0);
 
         const matrix = transformer.fromLatLngAltitude(pos);
-
         camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
-
-        webglOverlayView.requestRedraw();
+        webglOverlayView?.requestRedraw();
         renderer.render(scene, camera);
         renderer.resetState();
       };
-
+      webglOverlayView.onRemove = () => {
+        webglOverlayView = null;
+      };
       webglOverlayView.setMap(map);
     }
 
+    const handleCompass = (e: any) => (compass = 360 - e.alpha);
     if (!ref.current) return;
-    window.addEventListener(
-      'deviceorientationabsolute',
-      (e: any) => (compass = 360 - e.alpha),
-      true
-    );
+    window.addEventListener('deviceorientationabsolute', handleCompass, true);
     map = new google.maps.Map(ref.current, MAP_OPTION);
     initWebglOverlayView(map);
+    return () => {
+      webglOverlayView?.setMap(null);
+      window.removeEventListener('deviceorientationabsolute', handleCompass);
+    };
   }, []);
+  console.log('hello');
   const handleCenter = () => {
     map.moveCamera({ center: CENTER });
+    console.log(CENTER);
   };
 
   return (
     <>
       <div ref={ref} id='map' style={{ height: '90vh', width: '100vw' }}></div>
       <button onClick={handleCenter}>to Center</button>
+      <Link to={'/'}>home</Link>
     </>
   );
 }
